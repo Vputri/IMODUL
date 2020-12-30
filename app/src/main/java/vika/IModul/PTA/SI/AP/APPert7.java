4f41674bd098355e4fefb7adaf4f8d18d4682958
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2020-12-30 23:09:50 +0700

    commit keempat

---
+++ app/src/main/java/vika/IModul/PTA/SI/AP/APPert7.java
@@ -0,0 +1,14 @@
+package vika.IModul.PTA.SI.AP;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class APPert7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appert7);
+    }
+}
